72f72633510dad4d274f4c37264e2427725816f7
Rixium <gamerpotty@gmail.com>
2020-10-13 19:24:52 +0100

    Add basic scene manager implementation

---
+++ src/View/Scenes/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectSanctuary.View.Scenes
+{
+    internal class SceneManager : ISceneManager
+    {
+        private HashSet<IScene> _scenes = new HashSet<IScene>();
+        private IScene _activeScene;
+        private IScene _nextScene;
+
+        public void AddScene(IScene scene) => _scenes.Add(scene);
+
+        public void SetScene<T>() where T : IScene =>
+            _nextScene = _scenes.FirstOrDefault(scene => scene.GetType() == typeof(T));
+
+        public void Update()
+        {
+            _activeScene.Update();
+        }
+
+        public void Draw()
+        {
+            _activeScene.Draw();
+        }
+    }
+}
