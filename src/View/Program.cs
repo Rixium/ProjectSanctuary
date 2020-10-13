c25b16d7c019fa101de920f5f937a7f382dc9395
Rixium <gamerpotty@gmail.com>
2020-10-13 18:12:29 +0100

    Add view project

---
+++ src/View/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjectSanctuary.View
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
