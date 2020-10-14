3832a317f946a6497c115bc1d883e98569cb3f66
Rixium <gamerpotty@gmail.com>
2020-10-14 13:45:10 +0100

    Add wrapper for monogame content manager

---
+++ src/View/Content/MonoGameContentManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace ProjectSanctuary.View.Content
+{
+    internal class MonoGameContentManager : IContentManager
+    {
+        private readonly ContentManager _contentManager;
+
+        public MonoGameContentManager(ContentManager contentManager)
+        {
+            _contentManager = contentManager;
+        }
+
+        public T Load<T>(string assetName) => _contentManager.Load<T>(assetName);
+
+        public void Unload() => _contentManager.Unload();
+    }
+}
