120ae9d4421cc74ee806ad3adfc702a4b4f7b809
Rixium <gamerpotty@gmail.com>
2020-10-14 12:31:29 +0100

    Add SplashScene

---
+++ src/View/Scenes/SplashScene.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectSanctuary.View.Scenes
+{
+    internal class SplashScene : IScene
+    {
+        public void Update()
+        {
+            
+        }
+
+        public void Draw()
+        {
+            
+        }
+    }
+}
