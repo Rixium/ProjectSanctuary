835257ae79dbfb118ca7e2a1fe1ce85fbb34defe
Rixium <gamerpotty@gmail.com>
2020-10-14 21:13:37 +0100

    Draw splash screen

--- src/View/Scenes/SplashScene.cs
+++ src/View/Scenes/SplashScene.cs
@@ -12,12 +12,20 @@ namespace ProjectSanctuary.View.Scenes
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin();
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
-            spriteBatch.Draw(
-                ContentChest.Instance.Get<Texture2D>("splash"),
-                new Rectangle(0, 0, ViewManager.ViewPort.Width, ViewManager.ViewPort.Height),
-                Color.White);
+            var texture = ContentChest.Instance.Get<Texture2D>("splash");
+            var zoom = ViewManager.ViewPort.Height < 800 ? 2 : 3;
+            
+            spriteBatch.Draw(texture,
+                new Vector2(ViewManager.ViewPort.Width, ViewManager.ViewPort.Height) / 2,
+                new Rectangle(0, 0, texture.Width, texture.Height),
+                Color.White,
+                0f,
+                new Vector2(texture.Width / 2.0f, texture.Height / 2.0f),
+                zoom,
+                SpriteEffects.None,
+                0.2f);
 
             spriteBatch.End();
         }
